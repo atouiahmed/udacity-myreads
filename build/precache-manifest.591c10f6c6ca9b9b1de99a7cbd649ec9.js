706d318c1859b1a5c9d9deeb6b664c0d13a132d2
Ahmed Atoui <a.atoui@altqniah.sa>
2021-07-04 21:49:42 +0100

    build project files

---
+++ build/precache-manifest.591c10f6c6ca9b9b1de99a7cbd649ec9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c2c2ca1355677cea222",
+    "url": "/static/css/main.88bb50c1.chunk.css"
+  },
+  {
+    "revision": "3c2c2ca1355677cea222",
+    "url": "/static/js/main.3c2c2ca1.chunk.js"
+  },
+  {
+    "revision": "e5258190030b12c87c19",
+    "url": "/static/js/1.e5258190.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "d90a0f31be898940bad31d8525ed0309",
+    "url": "/index.html"
+  }
+];
